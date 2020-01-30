7df008cf49463568074b6413dd8de43df4617e89
Buzhen YE <buzhen.ye@imperial.ac.uk>
2020-01-30 21:05:33 +0000

    Add add_clause and delete_clause

--- func_generator.py
+++ func_generator.py
@@ -33,3 +33,28 @@ print(swap_between_clauses(['1 2 3 0', '4 5 6']))
 
 def add_clause():
     print()
+
+
+def add_clause(no_of_vars, data):
+    no_of_clauses = len(data)
+    no_of_new_clauses = random.randint(1, no_of_clauses)
+
+    for i in range(0, no_of_new_clauses):
+        line = ''
+        for _ in range(0, random.randint(1, no_of_vars)):
+            if random.randint(0,1) == 1:
+                line += '-'
+            line = line + str(random.randint(1, no_of_vars)) + ' '
+        line += '0'
+        data.append(line)
+    
+    return data
+
+
+def delete_clause(data):
+    no_of_clauses = len(data)
+
+    for _ in range(0, no_of_clauses):
+        del data[random.randint(0, len(data) - 1)]
+
+    return data
